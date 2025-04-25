0144b8dc61ae75a2204bc002a29b5af44e7aaf14
HyeonSeung Oh <77887324+raymond1203@users.noreply.github.com>
2025-04-25 14:50:39 +0900

    Bronze 5

---
+++ Class1/2557.cpp
@@ -0,0 +1,29 @@
+/*
+ * 문제: 백준 2557번 - Hello World
+ * 링크: https://www.acmicpc.net/problem/2557
+ * 분류: 구현, 입출력
+ * 
+ * 문제 설명:
+ * - "Hello World!"를 출력하는 가장 기본적인 출력 문제.
+ * - 입력은 없으며, 출력 형식에 맞게 정확히 출력해야 한다.
+ * 
+ * 입력:
+ * - 없음
+ * 
+ * 출력:
+ * - "Hello World!" (따옴표 제외)을 한 줄에 출력한다.
+ * 
+ * 접근 방법:
+ * - C++ 표준 출력 함수인 cout을 사용하여 문자열을 그대로 출력한다.
+ * 
+ * 시간복잡도: O(1)
+ * 공간복잡도: O(1)
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World!";
+    return 0;
+}
